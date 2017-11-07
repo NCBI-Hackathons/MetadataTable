0d52ab26a23d8a4beabc6843515cb23413939b0a
Ubuntu <ubuntu@ip-172-16-242-86.ec2.internal>
2017-11-07 22:51:57 +0000

    xml

--- parse.py
+++ parse.py
@@ -7,14 +7,18 @@ def _return_file_content(file_or_string_to_parse):
     else:
         return file_or_string_to_parse.read()
 
+def get_experiment_xml_string(xml_content):
+    outer_xml = re.split("</?EXPERIMENT_PACKAGE_SET>", xml_content)
+    experiments = re.split("<EXPERIMENT_PACKAGE>", outer_xml[1].strip())
+    return experiments[1:]
+
 
 def parse(file_or_string_to_parse):
     data_array = []
     exp_dict = {}
     xml_content = _return_file_content(file_or_string_to_parse)
-    outer_xml = re.split("</?EXPERIMENT_PACKAGE_SET>", xml_content)
-    experiments = re.split("<EXPERIMENT_PACKAGE>", outer_xml[1].strip())
-    for experiment in experiments[1:]:
+    experiments = get_experiment_xml_string(xml_content)
+    for experiment in experiments:
         experiment = "<EXPERIMENT_PACKAGE>" + experiment
         tree = etree.fromstring(experiment)
         runs = tree.findall(".//RUN_SET/RUN")
@@ -26,7 +30,7 @@ def parse(file_or_string_to_parse):
                     list_of_values.append(run.find(field[0]).attrib.get(field[1], "NA"))
                 else:
                     list_of_values.append(run.find(field).text)
-            yield list_of_values, experiment
+            yield list_of_values
 
 sra = open("sra1000.xml", "r")
 parse_func = (parse(sra))
