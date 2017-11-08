baba776499ebf7f636c5190a3375a05a24ab51f6
tarat44 <lw20853@yahoo.com>
2017-11-08 16:56:23 +0000

    changed experiment generation

--- metaparse.py
+++ metaparse.py
@@ -11,9 +11,12 @@ def _return_file_content(file_or_string_to_parse):
 def get_experiment_xml_string(xml_content):
     """"returns an xml string of experiments
         outside the experiment package set root"""
+    corrected_experiments = []
     outer_xml = re.split("</?EXPERIMENT_PACKAGE_SET>", xml_content)
     experiments = re.split("<EXPERIMENT_PACKAGE>", outer_xml[1].strip())
-    return experiments[1:]
+    for experiment in experiments[1:]:
+        corrected_experiments.append("<EXPERIMENT_PACKAGE>" + experiment)
+    return corrected_experiments
 
 
 def parse(file_or_string_to_parse, xpath_list):
@@ -22,7 +25,6 @@ def parse(file_or_string_to_parse, xpath_list):
     xml_content = _return_file_content(file_or_string_to_parse)
     experiments = get_experiment_xml_string(xml_content)
     for experiment in experiments:
-        experiment = "<EXPERIMENT_PACKAGE>" + experiment
         tree = etree.fromstring(experiment) #loads each experiment into a tree
         runs = tree.findall(".//RUN_SET/RUN") #obtains all runs from the experiment
         list_of_values = []
